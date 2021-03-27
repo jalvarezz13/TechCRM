7910adc28ba6368af5121c74f5c8e275ab0517d5
alberto-diaz15 <alberto.diaz15@alu.uclm.es>
2021-03-27 13:55:22 +0100

    prueba

--- app/src/main/java/com/example/crm_alvarez_diaz/MainActivity.java
+++ app/src/main/java/com/example/crm_alvarez_diaz/MainActivity.java
@@ -17,7 +17,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
-
     private AppBarConfiguration mAppBarConfiguration;
 
     @Override
